39b96b288e49b94886c0f27876ce8e6babc9adb7
Andreas Kohn <andreas.kohn@gmail.com>
2018-06-30 14:10:05 +0200

    Restore the indentation using tabs
    This got lost in the conversion to Polymer 3.x.

--- paper-collapse-group.js
+++ paper-collapse-group.js
@@ -1,14 +1,14 @@
-import { html, PolymerElement } from '@polymer/polymer/polymer-element.js';
+import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
 import {FlattenedNodesObserver} from '@polymer/polymer/lib/utils/flattened-nodes-observer.js';
 
 /**
  * `PaperCollapseGroup`
- * 
+ *
  * An element which is able to group a set of paper-collapse-items , where only one
  * item can be opened.
- * 
+ *
  * ### Example
- * 
+ *
  * ```html
  * <paper-collapse-group>
  * <paper-collapse-item icon="icons:favorite" header="Item 1" opened>
@@ -22,84 +22,84 @@ import {FlattenedNodesObserver} from '@polymer/polymer/lib/utils/flattened-nodes
  * </paper-collapse-item>
  * </paper-collapse-group>
  * ```
- * 
+ *
  * @customElement
  * @polymer
  * @demo demo/index.html
  */
 class PaperCollapseGroup extends PolymerElement {
 
-  static get template() {
-    return html`<slot id="content"></slot>`;
-  }
+	static get template() {
+		return html`<slot id="content"></slot>`;
+	}
 
-  static get properties() {
-    return {
-      /**
-       * Array of observed paper-collapse-items
-       *
-       * @type {Array}
-       */
-      _items: {
-        type: Array,
-        notify: true,
-        value: []
-      }
-    };
-  }
+	static get properties() {
+		return {
+			/**
+			 * Array of observed paper-collapse-items
+			 *
+			 * @type {Array}
+			 */
+			_items: {
+				type: Array,
+				notify: true,
+				value: []
+			}
+		};
+	}
 
-  /**
-   * Use for one-time configuration of your component after local DOM is
-   * initialized.
-   */
-  ready() {
-    super.ready();
-    this.addEventListener('toggle', e => this._onToggle(e));
-  }
+	/**
+	 * Use for one-time configuration of your component after local DOM is
+	 * initialized.
+	 */
+	ready() {
+		super.ready();
+		this.addEventListener('toggle', e => this._onToggle(e));
+	}
 
-  connectedCallback() {
-    super.connectedCallback();
-    this._observer = new FlattenedNodesObserver(this.$.content, info => {
+	connectedCallback() {
+		super.connectedCallback();
+		this._observer = new FlattenedNodesObserver(this.$.content, info => {
 
-      var addedItems = info.addedNodes.filter(node => {
-        return node.nodeName.toLowerCase() === 'paper-collapse-item';
-      });
+			var addedItems = info.addedNodes.filter(node => {
+				return node.nodeName.toLowerCase() === 'paper-collapse-item';
+			});
 
-      if (addedItems.length > 0) {
-        this._items = this._items.concat(addedItems);
+			if (addedItems.length > 0) {
+				this._items = this._items.concat(addedItems);
 
-        const updateEvent = new CustomEvent('update', {
-          detail: null,
-          bubbles: true,
-          composed: true
-        });
+				const updateEvent = new CustomEvent('update', {
+					detail: null,
+					bubbles: true,
+					composed: true,
+				});
 
-        this.dispatchEvent(updateEvent);
-      }
-    });
-  }
+				this.dispatchEvent(updateEvent);
+			}
+		});
+	}
 
-  disconnectedCallback() {
-    super.disconnectedCallback();
-    this._observer.disconnect();
-  }
+	disconnectedCallback() {
+		super.disconnectedCallback();
+		this._observer.disconnect();
+	}
 
-  /**
-   * On Toggle
-   *
-   * Listen to fired toggle events from items and update the groups
-   *
-   * @param  {Event} e
-   */
-  _onToggle(e) {
-    this._items.forEach(item => {
-      // Force all other items (aside the one triggering the event) to
-      // get closed
-      if (item !== e.target) {
-        item.opened = false;
-      }
-    });
-  }
+	/**
+	 * On Toggle
+	 *
+	 * Listen to fired toggle events from items and update the groups
+	 *
+	 * @param  {Event} e
+	 */
+	_onToggle(e) {
+		this._items.forEach(item => {
+			// Force all other items (aside the one triggering the event) to
+			// get closed
+			if (item !== e.target) {
+				item.opened = false;
+			}
+		});
+	}
 }
 
 window.customElements.define('paper-collapse-group', PaperCollapseGroup);
--- paper-collapse-item.js
+++ paper-collapse-item.js
@@ -9,11 +9,11 @@ import '@polymer/paper-styles/paper-styles.js';
 
 /**
  * `PaperCollapseItem`
- * 
+ *
  * A Material Design [item with header and collapsible content](https://www.google.com/design/spec/components/lists.html)
- * 
+ *
  * ### Example
- * 
+ *
  * ```html
  * <paper-collapse-item icon="icons:favorite" header="Item 1" opened>
  *  Lots of very interesting content.
@@ -26,7 +26,7 @@ import '@polymer/paper-styles/paper-styles.js';
  * </paper-collapse-item>
  * ```
  * ### Styling
- * 
+ *
  * The following custom properties and mixins are available for styling:
  * Custom property | Description | Default
  * ----------------|-------------|----------
@@ -34,126 +34,126 @@ import '@polymer/paper-styles/paper-styles.js';
  * --paper-collapse-item-content|Mixin applied to collapsible content|{}
  * --paper-collapse-item-icon|Mixin applied to icon|{}
  * --paper-collapse-simple-paper-item-styles|Mixin applied to simple-paper-item|{}
- * 
+ *
  * @customElement
  * @polymer
  * @demo demo/index.html
  */
 class PaperCollapseItem extends PolymerElement {
-  static get template() {
-    return html`
-        <style>
-            .header {
-                min-height: 48px;
-                color: var(--primary-text-color);
-                @apply --layout-horizontal;
-                @apply --layout-center;
-                @apply --paper-font-subhead;
-                @apply --paper-collapse-item-header;
-            }
-            .icon {
-                margin-right: 24px;
-                --iron-icon-height: 32px;
-                --iron-icon-width: 32px;
-                @apply --paper-collapse-item-icon;
-            }
-            .icon, .toogle {
-                color: var(--disabled-text-color);
-            }
-            .html {
-                @apply --layout-flex;
-            }
-            .content {
-                color: var(--primary-text-color);
-                @apply --paper-font-body1;
-                @apply --paper-collapse-item-content;
-            }
-            
-            simple-paper-item {
-                @apply --paper-collapse-simple-paper-item-styles;
-            }
-        </style>
+	static get template() {
+		return html`
+			<style>
+				.header {
+					min-height: 48px;
+					color: var(--primary-text-color);
+					@apply --layout-horizontal;
+					@apply --layout-center;
+					@apply --paper-font-subhead;
+					@apply --paper-collapse-item-header;
+				}
+				.icon {
+					margin-right: 24px;
+					--iron-icon-height: 32px;
+					--iron-icon-width: 32px;
+					@apply --paper-collapse-item-icon;
+				}
+				.icon, .toogle {
+					color: var(--disabled-text-color);
+				}
+				.html {
+					@apply --layout-flex;
+				}
+				.content {
+					color: var(--primary-text-color);
+					@apply --paper-font-body1;
+					@apply --paper-collapse-item-content;
+				}
 
-        <simple-paper-item>
-            <paper-item-body>
-                <div class="header" on-tap="_toggleOpened">
-                    <template is="dom-if" if="[[_or(icon, src)]]">
-                        <iron-icon class="icon" src="[[src]]" icon="[[icon]]"></iron-icon>
-                    </template>
-                    <template is="dom-if" if="[[header]]">
-                        <div class="html" inner-h-t-m-l="[[header]]"></div>
-                    </template>
-                    <slot class="html" name="header"></slot>
-                    <paper-icon-button class="toggle" icon="[[_toggleIcon]]"></paper-icon-button>
-                </div>
-                <iron-collapse class="content" opened="{{opened}}">
-                    <slot></slot>
-                </iron-collapse>
-            </paper-item-body>
-        </simple-paper-item>
-      `;
-  }
+				simple-paper-item {
+					@apply --paper-collapse-simple-paper-item-styles;
+				}
+			</style>
 
-  static get properties() {
-    return {
-      /**
-       * Text in the header row
-       */
-      header: String,
+			<simple-paper-item>
+				<paper-item-body>
+					<div class="header" on-tap="_toggleOpened">
+						<template is="dom-if" if="[[_or(icon, src)]]">
+							<iron-icon class="icon" src="[[src]]" icon="[[icon]]"></iron-icon>
+						</template>
+						<template is="dom-if" if="[[header]]">
+							<div class="html" inner-h-t-m-l="[[header]]"></div>
+						</template>
+						<slot class="html" name="header"></slot>
+						<paper-icon-button class="toggle" icon="[[_toggleIcon]]"></paper-icon-button>
+					</div>
+					<iron-collapse class="content" opened="{{opened}}">
+						<slot></slot>
+					</iron-collapse>
+				</paper-item-body>
+			</simple-paper-item>
+			`;
+	}
 
-      /**
-       * The name of the icon to use. The name should be of the
-       * form: iconset_name:icon_name.
-       */
-      icon: String,
+	static get properties() {
+		return {
+			/**
+			 * Text in the header row
+			 */
+			header: String,
 
-      /**
-       * If using paper-collapse-item without an iconset, you can set the
-       * src to be the URL of an individual icon image file. Note that
-       * this will take precedence over a given icon attribute.
-       */
-      src: String,
+			/**
+			 * The name of the icon to use. The name should be of the
+			 * form: iconset_name:icon_name.
+			 */
+			icon: String,
 
-      /**
-       * True if the content section is opened
-       */
-      opened: {
-        type: Boolean,
-        reflectToAttribute: true,
-        notify: true
-      },
+			/**
+			 * If using paper-collapse-item without an iconset, you can set the
+			 * src to be the URL of an individual icon image file. Note that
+			 * this will take precedence over a given icon attribute.
+			 */
+			src: String,
 
-      _toggleIcon: {
-        type: String,
-        computed: '_computeToggleIcon(opened)'
-      }
-    };
-  }
+			/**
+			 * True if the content section is opened
+			 */
+			opened: {
+				type: Boolean,
+				reflectToAttribute: true,
+				notify: true
+			},
 
-  /**
-   * Fired whenever the status is changed (opened/closed)
-   *
-   * @event toggle
-   */
-  _toggleOpened(e) {
-    this.opened = !this.opened;
+			_toggleIcon: {
+				type: String,
+				computed: '_computeToggleIcon(opened)'
+			}
+		};
+	}
 
-    const toggleEvent = new CustomEvent('toggle', {
-      detail: this,
-      bubbles: true,
-      composed: true
-    });
+	/**
+	 * Fired whenever the status is changed (opened/closed)
+	 *
+	 * @event toggle
+	 */
+	_toggleOpened(e) {
+		this.opened = !this.opened;
 
-    this.dispatchEvent(toggleEvent);
-  }
+		const toggleEvent = new CustomEvent('toggle', {
+			detail: this,
+			bubbles: true,
+			composed: true,
+		});
 
-  _computeToggleIcon(opened) {
-    return opened ? 'icons:expand-less' : 'icons:expand-more';
-  }
+		this.dispatchEvent(toggleEvent);
+	}
 
-  _or(value1, value2) {
-    return value1 || value2;
-  }
+	_computeToggleIcon(opened) {
+		return opened ? 'icons:expand-less' : 'icons:expand-more';
+	}
+
+	_or(value1, value2) {
+		return value1 || value2;
+	}
 }
 
 window.customElements.define('paper-collapse-item', PaperCollapseItem);
